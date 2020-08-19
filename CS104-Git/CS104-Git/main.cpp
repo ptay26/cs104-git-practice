c0708fee7f96f282475c9f0d5da4d0a4ed51abe5
Pablo Tayun-Mazariegos <tayunmaz@usc.edu>
2020-08-19 10:55:24 -0700

    Add CS104-Git/CS104-Git/main.cpp with some code

---
+++ CS104-Git/CS104-Git/main.cpp
@@ -0,0 +1,16 @@
+//
+//  main.cpp
+//  CS104-Git
+//
+//  Created by Pablo Tayun on 8/19/20.
+//  Copyright © 2020 Pablo Tayun. All rights reserved.
+//
+
+#include <iostream>
+using namespace std;
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    cout << "Hello, World!\n";
+    return 0;
+}
